e08427dda4e217238b5daa794fb0c42a21e4d7c9
Vanadylxu <vanadylxu@sjtu.edu.cn>
2021-06-19 23:37:15 +0800

    补充后端文件以及更新文档，添加pre

---
+++ 大作业/analysis/analysis.py
@@ -0,0 +1,11 @@
+import numpy as np
+import time
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+from matplotlib.font_manager import FontProperties
+import seaborn as sns
+import os
+
+data = pd.read_csv('../pre_deal/all_final.csv', encoding='utf-8')
